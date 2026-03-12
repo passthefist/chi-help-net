e6f4bfca30e07524e02936e3f33294ac9c196f5d
honeybody <honeybody@protonmail.com>
2026-03-12 18:13:42 -0500

    new drawer

--- _data/devLinks.js
+++ _data/devLinks.js
@@ -1,1 +1,1 @@
-export default [{ props: { label: "404", type: "link", size: "sm" }, attrs: { href: "/404" } }];
+export default [{ props: { label: "404", type: "link", size: "sm", variant: "link" }, attrs: { href: "/404" } }];
